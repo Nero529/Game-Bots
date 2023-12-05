69af30c86ed81ab09b5c64290cfa272607183d0c
Quamez Anderson <quamezmanderson@gmail.com>
2023-12-04 19:55:09 -0500

    Barter bot: Sorted .pngs into separate folder and added some code for automation process

--- BDO/master_barterer/master_barterer.py
+++ BDO/master_barterer/master_barterer.py
@@ -4,6 +4,7 @@ import pydirectinput
 import cv2
 import win32gui
 import time
+import pandas as pd
 
 class Client:
 
@@ -90,7 +91,7 @@ def open_barters():
     pydirectinput.press('esc')
     time.sleep(0.3)
     pyautogui.screenshot(game_img)
-    x,y = bdo.match_image(game_img, 'BDO/master_barterer/barter_icon.png')
+    x,y = bdo.match_image(game_img, 'barter_icon.png')
     click(x,y)
     time.sleep(0.3)
 
@@ -104,33 +105,33 @@ def get_route(item_name, tier):
         time.sleep(0.5)
         click(1100,400)
     elif tier == 2:
-        x,y = bdo.match_image(game_img, 'BDO/master_barterer/tier2_trades.png')
+        x,y = bdo.match_image(game_img, 'screenshots/tier2_trades.png')
         click(x,y)
         time.sleep(0.5)
     elif tier == 3:
-        x,y = bdo.match_image(game_img, 'BDO/master_barter/tier3_trades.png')
+        x,y = bdo.match_image(game_img, 'screenshots/tier3_trades.png')
         click(x,y)
         time.sleep(0.5)
 
     elif tier == 4:
-        x,y = bdo.match_image(game_img, 'BDO/master_barterer/tier4_trades.png')
+        x,y = bdo.match_image(game_img, 'screenshots/tier4_trades.png')
         click(x,y)
         time.sleep(0.5)
 
     elif tier == 5:
-        x,y = bdo.match_image(game_img, 'BDO/master_barterer/tier5_trades.png')
+        x,y = bdo.match_image(game_img, 'screenshots/tier5_trades.png')
         click(x,y)
         time.sleep(0.5)
 
     pyautogui.screenshot(game_img)
-    x1,y1 = bdo.match_image(game_img, 'BDO/master_barterer/search.png')
+    x1,y1 = bdo.match_image(game_img, 'screenshots/search.png')
     click(x1-100,y1)
     time.sleep(1)
     for x in item_name:
         pydirectinput.press(x)
     pydirectinput.press('enter')
     pyautogui.screenshot(game_img)
-    x,y = bdo.match_image(game_img, 'BDO/master_barterer/navigate_button.png')
+    x,y = bdo.match_image(game_img, 'screenshots/navigate_button.png')
     pyautogui.moveTo(x,y)
     time.sleep(0.5)
     click(x,y)
@@ -144,21 +145,21 @@ def get_route(item_name, tier):
 
 def barter(quantity):
     pyautogui.screenshot(game_img)
-    while not bdo.match_image(game_img, 'BDO/master_barterer/anchor_button.png'):
+    while not bdo.match_image(game_img, 'screenshots/anchor_button.png'):
         time.sleep(1)
         pyautogui.screenshot(game_img)
     pydirectinput.press('ctrlleft')
     time.sleep(1)
-    x,y = bdo.match_image(game_img, 'BDO/master_barterer/anchor_button.png')
+    x,y = bdo.match_image(game_img, 'screenshots/anchor_button.png')
     click(x,y)
     pyautogui.moveTo(500,500)
     time.sleep(0.5)
     pyautogui.screenshot(game_img)
-    x,y = bdo.match_image(game_img, 'BDO/master_barterer/barter_button.png')
+    x,y = bdo.match_image(game_img, 'screenshots/barter_button.png')
     click(x,y)
     time.sleep(0.5)
     pyautogui.screenshot(game_img)
-    x,y = bdo.match_image(game_img, 'BDO/master_barterer/exchange.png')
+    x,y = bdo.match_image(game_img, 'screenshots/exchange.png')
     click(x,y)
     time.sleep(0.5)
     pydirectinput.press(quantity)
@@ -166,49 +167,85 @@ def barter(quantity):
     pydirectinput.press('esc')
     time.sleep(0.5)
     pyautogui.screenshot(game_img)
-    x,y = bdo.match_image(game_img, 'BDO/master_barterer/depart_button.png')
+    x,y = bdo.match_image(game_img, 'screenshots/depart_button.png')
     click(x,y)
     pydirectinput.keyDown('s')
     time.sleep(20)
     pydirectinput.keyUp('s')
 
+def read_config(file): # Reads config file for auto status and max_quantities desired
+    file_opener = open(file)
+    max_items = []
+    auto = False
+    for index, x in enumerate(file_opener):
+        if index != 0:
+            cap = int(x.split(":")[1])
+            max_items.append(cap)
+        else:
+            if "Y" in x:
+                auto = True
+            
+    return auto, max_items
 
+def look_up_item(item_name, html_file): # Returns database item name, tier, and quantity
+    barter_data = pd.read_html(html_file)[0]
+    name_columns = ["B","E","H","K","N"]
+    for tier, x in enumerate(name_columns):
+        for index, row in enumerate(barter_data[x]):
+            if index < 16:
+                if type(row) is float: # Skips over NaN values for empty cells
+                    pass
+                else:
+                    # Returns the full item name, tier of item, and quantity held
+                    # chr(ord(x) + 1) used to convert column to ascii and then look one over for quantity
+                    if item_name in row.lower():
+                        return row.lower(), tier + 1, barter_data[chr(ord(x) + 1)][index] 
+            else:
+                break
+    return False
 
-
-raw_item_list = input("Enter name of items in barter route: ")
-barter_item_list = []
-item_name = ''
-for index, x in enumerate(raw_item_list):
-    if x != ',':
-        item_name += x
+def get_barter_quantity(tier): # Returns maximum barters for a given tier
+    if tier !=5:
+        return 10
     else:
-        barter_item_list.append(item_name)
-        item_name = ''
+        return 6
 
-    if index == len(raw_item_list) - 1 :
-        barter_item_list.append(item_name)
 
-raw_quantity = input("Enter number of items to trade: ")
-item_quantity = []
-num_item = ''
-for index, x in enumerate(raw_quantity):
-    if x != ',':
-        num_item += x
-    else:
-        item_quantity.append(num_item)
-        num_item = ''
-    if index == len(raw_quantity) - 1 :
-        item_quantity.append(num_item)
-print(barter_item_list,item_quantity)
-bdo = Client()
-bdo.register_window()
-bdo.set_active()
-game_img = 'BDO/master_barterer/game_screen.png'
-tier = 2
-for index,item in enumerate(barter_item_list):
-    open_barters()
-    get_route(item, tier)
-    barter(item_quantity[index])
-    tier += 1
-    if tier == 6:
-        tier = 1
+# Used for getting the player's desired max quantity for each tier of item (to be used for auto checking barter info)
+#max_items = read_config("config.txt") 
+html_file = 'Barter_Stock.html'
+test = False
+if not test:
+    raw_item_list = input("Enter name of items in barter route: ")
+    barter_item_list = []
+    item_quantity = []
+    item_name = ''
+    for index, x in enumerate(raw_item_list):
+        if x != ',':
+            item_name += x
+        else:
+            if not look_up_item(item_name, html_file):
+                print(item_name + " was not found. Terminating program")
+            else:
+                barter_item_list.append(look_up_item(item_name, html_file)[0])
+                item_name = ''
+
+        if index == len(raw_item_list) - 1 :
+            if not look_up_item(item_name, html_file):
+                print(item_name + " was not found. Terminating program")
+            else:
+                barter_item_list.append(look_up_item(item_name, html_file)[0])
+
+    if len(barter_item_list) > 0:
+        bdo = Client()
+        bdo.register_window()
+        bdo.set_active()
+        game_img = 'screenshots/game_screen.png'
+        tier = 2
+        for index,item in enumerate(barter_item_list):
+            open_barters()
+            get_route(item, tier)
+            barter(item_quantity[index])
+            tier += 1
+            if tier == 6:
+                tier = 1
